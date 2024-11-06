62c6bcacf3b0fe9000ee68f840438f700858c930
jtp-sarthak-bhan <Sarthak.Bhan@jtp.co.jp>
2024-11-06 14:31:12 +0900

    Initial commit

---
+++ test/source-account.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SourceAccount from '../lib/source-account-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/source-account-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SourceAccount.SourceAccountStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
